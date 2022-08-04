2a9a7d3ca01cc7cd109b9c4722910f38cecfc167
Cosmin Craciun <cosmin.craciun@cwavesoftware.com>
2022-08-04 19:38:43 +0300

    Working update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,18 +5,24 @@ if [ $# -lt 3 ]; then
     exit 1
 fi
 
-sh ../automation/login.sh $1 $2 $3
-running_scans=$(curl -k -b cookiejar -s $1/api/listScanHistory/ | jq '.scan_histories[] | .scan_status' | grep -e "^1$")
-echo $running_scans
+bash ./automation/login.sh $1 $2 $3 && \
+running_scans=$(curl -k -b cookiejar -s $1/api/listScanHistory/ | jq '.scan_histories[] | .scan_status' | grep -e "^1$") && \
+echo $running_scans && \
 [[ $running_scans != "" ]] && echo "Scans are still running" && exit -1
 
-pending_scans=$(curl -k -b cookiejar -s $1/api/listScanHistory/ | jq '.scan_histories[] | .scan_status' | grep -e "^-1$")
-echo $pending_scans
+pending_scans=$(curl -k -b cookiejar -s $1/api/listScanHistory/ | jq '.scan_histories[] | .scan_status' | grep -e "^-1$") && \
+echo $pending_scans && \
 [[ $pending_scans != "" ]] && echo "Scans are still pending" && exit -1
 
-cd ..
+git config --global --add safe.directory /home/cwave/Applications/rengine
+eval $(ssh-agent) &&
+ssh-add /home/cwave/.ssh/github_rsa
+
 out=$(git pull)
-[[ $out == "Already up to date." ]] && echo $out && exit -2
+[[ ! $? -eq 0 ]] && exit -1
+cat $out && \
+[[ $out == "Already up to date." ]] && exit -1
 echo "New code pulled from github"
 
-sudo docker-compose down && sudo docker-compose up -d --build
+docker-compose down
+docker-compose up -d --build
