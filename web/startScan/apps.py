bfa5c00aff8063b9414fc71f05a865d0538b91f7
Cosmin Craciun <cosmin.craciun@cwavesoftware.com>
2023-01-12 09:42:55 +0200

    Upgrading to go 1.19.5

--- web/startScan/apps.py
+++ web/startScan/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 import logging
 from reNgine import definitions
+import psycopg2
 
 class StartscanConfig(AppConfig):
     name = 'startScan'
@@ -11,6 +12,10 @@ class StartscanConfig(AppConfig):
         server restarted
         This does not include pending_scans, pending_scans are taken care by celery
         '''
-        logging.info('Aborting all the ongoing scans')
-        ScanHistory = self.get_model('ScanHistory')
-        ScanHistory.objects.filter(scan_status=definitions.SCAN_STATUS_IN_PROGRESS).update(scan_status=definitions.SCAN_STATUS_ABORTED)
+        try:
+            logging.info('Aborting all the ongoing scans')
+            ScanHistory = self.get_model('ScanHistory')
+            ScanHistory.objects.filter(scan_status=definitions.SCAN_STATUS_IN_PROGRESS).update(scan_status=definitions.SCAN_STATUS_ABORTED)
+        except Exception as e:
+            logging.error(e)
+            logging.info("Maybe first start???")
