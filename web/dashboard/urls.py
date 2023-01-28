f32f2afe53e4f4084a70d681346adcee3db80f64
Cosmin Craciun <cosmin.craciun@cwavesoftware.com>
2023-01-28 19:33:22 +0200

    Changing home page because of huge loading times of Dashboard

--- web/dashboard/urls.py
+++ web/dashboard/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
+    path('',
+        RedirectView.as_view(url='/target/list/target', permanent=False),
+    ),
     path(
-        '',
+        'dashboard/',
         views.index,
         name='dashboardIndex'),
     path(
