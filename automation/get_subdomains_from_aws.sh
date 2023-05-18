1e3f7ad50635120715eb283ce899273dbad470ba
Cosmin Craciun <cosmin.craciun@cwavesoftware.com>
2023-05-18 10:14:20 +0300

    Removing profile argument

--- automation/get_subdomains_from_aws.sh
+++ automation/get_subdomains_from_aws.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-aws route53 list-hosted-zones --profile $1 | jq '.HostedZones[].Id' | tr -d '"' | cut -d '/' -f3 > zones.lst
+aws route53 list-hosted-zones | jq '.HostedZones[].Id' | tr -d '"' | cut -d '/' -f3 > zones.lst
 rm subdomains.txt
-while read line; do echo "[+] getting zone id $line" && aws route53 --profile $1 list-resource-record-sets --hosted-zone-id $line | jq '.ResourceRecordSets[] | if (.Type=="A") then .Name else empty end' | grep $2 | tr -d '"' | sed 's/\.$//g' >> subdomains.txt; done < zones.lst
+while read line; do echo "[+] getting zone id $line" && aws route53 list-resource-record-sets --hosted-zone-id $line | jq '.ResourceRecordSets[] | if (.Type=="A") then .Name else empty end' | grep $1 | tr -d '"' | sed 's/\.$//g' >> subdomains.txt; done < zones.lst
