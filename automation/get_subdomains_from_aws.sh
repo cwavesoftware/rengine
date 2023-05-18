f1617f3e6ff74d6198ba4f7a0e5f9e159c045725
Cosmin Craciun <cosmin.craciun@cwavesoftware.com>
2023-05-18 10:48:19 +0300

    Downgrading logs size and fixing grep argument

--- automation/get_subdomains_from_aws.sh
+++ automation/get_subdomains_from_aws.sh
@@ -2,4 +2,4 @@
 
 aws route53 list-hosted-zones | jq '.HostedZones[].Id' | tr -d '"' | cut -d '/' -f3 > zones.lst
 rm subdomains.txt
-while read line; do echo "[+] getting zone id $line" && aws route53 list-resource-record-sets --hosted-zone-id $line | jq '.ResourceRecordSets[] | if (.Type=="A") then .Name else empty end' | grep $1 | tr -d '"' | sed 's/\.$//g' >> subdomains.txt; done < zones.lst
+while read line; do echo "[+] getting zone id $line" && aws route53 list-resource-record-sets --hosted-zone-id $line | jq '.ResourceRecordSets[] | if (.Type=="A") then .Name else empty end' | grep $(echo $1 | sed 's/\./\\./g') | tr -d '"' | sed 's/\.$//g' >> subdomains.txt; done < zones.lst
