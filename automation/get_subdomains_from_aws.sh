3db59ba090a2a0ae467f9fb5ca4246f66ebfca6c
cosmin-craciun-superbet <cosmin.craciun@happening.xyz>
2024-03-06 16:02:17 +0200

    --allow-empty-message

--- automation/get_subdomains_from_aws.sh
+++ automation/get_subdomains_from_aws.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/bash
-
-aws route53 list-hosted-zones | jq '.HostedZones[].Id' | tr -d '"' | cut -d '/' -f3 > zones.lst
+echo $#
+aws route53 list-hosted-zones | jq '.HostedZones[].Id' | tr -d '"' | cut -d '/' -f3 >zones.lst
 rm subdomains.txt
-while read line; do echo "[+] getting zone id $line" && aws route53 list-resource-record-sets --hosted-zone-id $line | jq '.ResourceRecordSets[] | if (.Type=="A") then .Name else empty end' | grep $(echo $1 | sed 's/\./\\./g') | tr -d '"' | sed 's/\.$//g' >> subdomains.txt; done < zones.lst
+rm tmp
+while read line; do
+	echo "[+] getting zone id $line" &&
+		aws route53 list-resource-record-sets --hosted-zone-id $line |
+		jq '.ResourceRecordSets[] | if (.Type=="A") then .Name else empty end' |
+			tr -d '"' | sed 's/\.$//g' >>tmp
+done <zones.lst
+
+[[ $# > 0 ]] && cat tmp | grep $(echo $1 | sed 's/\./\\./g') >>subdomains.txt
+[[ $# = 0 ]] && mv tmp subdomains.txt
